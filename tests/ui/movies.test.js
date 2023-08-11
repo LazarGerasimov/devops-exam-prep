53c9a4b32be409aa4ab83290047fdaba032dc4ad
Laz <lazgerasimov@gmail.com>
2023-08-11 23:33:21 +0100

    update ui tests URLs

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-    await page.goto('https://devops-exam-prep.onrender.com/');
+    await page.goto('https://devops-exam-prep.onrender.com');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
